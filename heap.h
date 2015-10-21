87c97111028a06eb90dcdaa89b5b1c9e5ea553eb
akazawa <a1211003@edu.cc.uec.ac.jp>
2015-10-21 14:51:42 +0900

    GetPrimaryFreeListSpace

--- heap.h
+++ heap.h
@@ -546,13 +546,21 @@ class Heap {
   // Returns the free list space that may contain movable objects (the
   // one that's not the non-moving space), either rosalloc_space_ or
   // dlmalloc_space_.
-  space::MallocSpace* GetPrimaryFreeListSpace() {
+  space::MallocSpace* GetPrimaryFreeListSpace(rep_flg = true) {
     if (kUseRosAlloc) {
-      DCHECK(rosalloc_space_ != nullptr);
-      // reinterpret_cast is necessary as the space class hierarchy
-      // isn't known (#included) yet here.
-      return reinterpret_cast<space::MallocSpace*>(rosalloc_space_);
-    } else {
+      switch(rep_flg){
+        case true:
+          DCHECK(rosalloc_space_ != nullptr);
+          // reinterpret_cast is necessary as the space class hierarchy
+          // isn't known (#included) yet here.
+          return reinterpret_cast<space::MallocSpace*>(rosalloc_space_);
+        case false:
+          DCHECK(rosalloc_space_ != nullptr);
+          // reinterpret_cast is necessary as the space class hierarchy
+          // isn't known (#included) yet here.
+          return reinterpret_cast<space::MallocSpace*>(rosalloc_space_);
+      }
+   } else {
       DCHECK(dlmalloc_space_ != nullptr);
       return reinterpret_cast<space::MallocSpace*>(dlmalloc_space_);
     }
